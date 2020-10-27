e39acd23b7221ea983746789c64b4f63d0e69966
arafat.ruet.bd <arafat.ruet.bd@gmail.com>
2020-10-28 00:08:10 +0600

    Init Commit

---
+++ build/precache-manifest.a24fd04b227a1d38763cf40334f13680.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a7a1c913f205fae4855bee8de11ae9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2dd3e0c11fbe1aab66c",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "ca59333e3daef08ca9e1",
+    "url": "/static/css/main.ee3ff526.chunk.css"
+  },
+  {
+    "revision": "b2dd3e0c11fbe1aab66c",
+    "url": "/static/js/2.47e4e9e1.chunk.js"
+  },
+  {
+    "revision": "65f5ddb972ec20d2f34381b91122a87f",
+    "url": "/static/js/2.47e4e9e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca59333e3daef08ca9e1",
+    "url": "/static/js/main.a717fd91.chunk.js"
+  },
+  {
+    "revision": "984ad03d8a37fd884621",
+    "url": "/static/js/runtime-main.a491203d.js"
+  },
+  {
+    "revision": "7da338800c7bdfe856c3d0ddc2b0d1b0",
+    "url": "/static/media/cheese.7da33880.png"
+  },
+  {
+    "revision": "e419b523e474070865b631ad7bf5faaa",
+    "url": "/static/media/logo.e419b523.png"
+  },
+  {
+    "revision": "aea7c54d8ca14a7f5ed49a326cd087e8",
+    "url": "/static/media/top.aea7c54d.png"
+  }
+]);
